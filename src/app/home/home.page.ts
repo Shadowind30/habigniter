ded8248f0993152938126c5e037ddcc61ac35e38
Carlos Sánchez <casw30rb@gmail.com>
2023-11-01 01:37:23 -0400

    feat(home): habit creation [non-persistent]

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { Component, ViewChild } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { IActivityItem } from '@shared/models';
+import { AlertsService } from '@shared/utilities/alerts.service';
+import {
+  getInitialDateISOString,
+  getRandomID,
+} from '@shared/utilities/helpers.functions';
 
 @Component({
   selector: 'app-home',
@@ -11,12 +15,41 @@ import { IActivityItem } from '@shared/models';
   imports: [SharedModule],
 })
 export class HomePage {
-  public activities: IActivityItem[] = [{
-    title: 'Read 20 pages of a book',
-    status: 'pending',
-    createdAt: '2020-01-01T00:00:00.000Z',
-    streak: 0,
-    id: '1'
-  }];
-  constructor() {}
+  @ViewChild('form') modal: HTMLIonModalElement;
+  public modalHeight = 260 / window.innerHeight;
+  public nameValue: string;
+  public errorMessage = 'El nombre debe de contener al menos 3 caracteres';
+
+  public activities: IActivityItem[] = [
+    {
+      title: 'Read 20 pages of a book',
+      status: 'pending',
+      createdAt: '2020-01-01T00:00:00.000Z',
+      streak: 0,
+      id: '1',
+    },
+  ];
+  constructor(private alertsService: AlertsService) {}
+
+  public async addActivity(): Promise<void> {
+    if (!this.checkValidity()) {
+      this.alertsService.simpleAlert(this.errorMessage);
+      return;
+    }
+
+    this.activities.push({
+      title: this.nameValue,
+      status: 'pending',
+      createdAt: getInitialDateISOString(),
+      streak: 0,
+      id: getRandomID(),
+    });
+
+    this.modal.dismiss();
+    this.nameValue = null;
+  }
+
+  private checkValidity(): boolean {
+    return this.nameValue && this.nameValue.length >= 3;
+  }
 }
