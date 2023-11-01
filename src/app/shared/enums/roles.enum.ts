9a50008b8725a04082889b2d1eaee99de6743943
Carlos Sánchez <casw30rb@gmail.com>
2023-11-01 01:34:58 -0400

    feat: add provider for alerts

---
+++ src/app/shared/enums/roles.enum.ts
@@ -0,0 +1,4 @@
+export enum RolesEnum {
+    CANCEL = 'CANCEL',
+    CONFIRM = 'CONFIRM'
+}
---
+++ src/app/shared/utilities/alerts.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { AlertButton, AlertController, AlertOptions } from '@ionic/angular';
+import { RolesEnum } from '@shared/enums/roles.enum';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AlertsService {
+  constructor(private alertCtrl: AlertController) {}
+
+  public async simpleAlert(
+    header: string,
+    subHeader?: string
+  ): Promise<HTMLIonAlertElement> {
+    const button = ['Entendido'];
+    const options = {
+      header,
+      subHeader,
+      buttons: button,
+    };
+    return this.alertBuilder({ ...options });
+  }
+
+  public async confirmationAlert(options: {
+    header: string;
+    subHeader?: string;
+    cancelText?: string;
+    confirmText?: string;
+  }): Promise<HTMLIonAlertElement> {
+    const { cancelText, confirmText, ...rest } = options;
+    const btnCancelText = cancelText || 'Cancelar';
+    const btnConfirmText = confirmText || 'Confirmar';
+
+    const buttons: AlertButton[] = [
+      {
+        text: btnCancelText,
+        role: RolesEnum.CANCEL,
+      },
+      {
+        text: btnConfirmText,
+        role: RolesEnum.CONFIRM,
+      },
+    ];
+
+    return this.alertBuilder({ ...rest, buttons });
+  }
+
+  /** Closes every alert that is active at the moment of calling*/
+  public async closeAll(): Promise<void> {
+    const topAlert = await this.alertCtrl.getTop();
+    if (topAlert) {
+      await topAlert.dismiss();
+      await this.closeAll();
+    }
+  }
+
+  private async alertBuilder(
+    options: AlertOptions
+  ): Promise<HTMLIonAlertElement> {
+    const alert = await this.alertCtrl.create(options);
+    await alert.present();
+    return alert;
+  }
+}
